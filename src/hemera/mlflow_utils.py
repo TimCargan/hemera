630c18072447a7a14bf84d62e16dcad94b3fb275
Tim Cargan <timcargan@gmail.com>
2023-11-03 13:22:52 +0000

    Better way to run MLflow
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/hemera/mlflow_utils.py
+++ src/hemera/mlflow_utils.py
@@ -2,34 +2,53 @@ import mlflow
 import os
 from absl import flags
 from flax.traverse_util import flatten_dict
+from typing import Optional
 
 from hemera.standard_logger import logging
 
+flags.DEFINE_string("exper_name", None, "MLFlow Experiment Name")
+flags.DEFINE_string("run_id", default=None, help="Slurm RunID")
+flags.DEFINE_list("module_filter", default=["absl", "tensorflow", "chex"], help="List of modules to filter out of logging")
 FLAGS = flags.FLAGS
 
 
-def ml_flow_track(f=None, /, *, expr_name, version):
-    experiment = mlflow.get_experiment_by_name(expr_name)
-    if not experiment:
-        mlflow.create_experiment(expr_name)
-    mlflow.set_experiment(expr_name)
+def ml_flow_track(f=None, /, *, expr_name: Optional[str] = None, **out_kwargs):
+    """Decorator to run MLFlow tracking.
 
+    :param f: Function to wrap
+    :param expr_name: The experiment name, Note: this is overwritten by FLAG.exper_name
+    :param kwargs: A KV dict of dict[str,str] of tags to log for the experimet
+    :return:
+    """
     def wrap(f):
         def new_main(*args, **kwargs):
-            # config = FLAGS.config
-            flag_dict = {k: {x.name: x.value for x in v} for k, v in list(FLAGS.flags_by_module_dict().items())}
+            # Set the exper name
+            exper_name = FLAGS.get_flag_value("expr_name", expr_name) # Use flag if set otherwise value passed in
+            experiment = mlflow.get_experiment_by_name(exper_name)
+            if not experiment:
+                mlflow.create_experiment(expr_name)
+            mlflow.set_experiment(expr_name)
+
             with mlflow.start_run(run_id=FLAGS.run_id) as run:
                 logging.info("run_id: %s", run.info.run_id)
 
                 if not FLAGS.run_id:
                     # Log params if a new exper
                     # TODO: else restore flag values?
+                    # TODO: Better way than logging all flag values
+                    # config = FLAGS.config
+                    flag_dict = {k: {x.name: x.value for x in v} for k, v in list(FLAGS.flags_by_module_dict().items())}
+                    # Filter out modules
+                    flag_dict = {k: v for k, v in flag_dict.items() if not any([k.startswith(m) for m in FLAGS.module_filter])}
                     config_flat_dict = {".".join(k).replace("/", "."): v for k, v in flatten_dict(flag_dict).items()}
                     mlflow.log_params(config_flat_dict)
-                    # FLAG TAGS?
-                    mlflow.set_tag("Version", version)
+
+                    # Log Tags
+                    for k, v in out_kwargs:
+                        mlflow.set_tag(k, v)
                     mlflow.set_tag("SLURM_ID", os.environ.get("SLURM_JOB_ID", "0"))
                 f(*args, **kwargs)
+
         return new_main
 
     # See if we're being called as @ml_flow_track or @ml_flow_track().
